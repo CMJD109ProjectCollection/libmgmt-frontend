d5198f8756a9250dad0345eb30749d12997f974d
ThanuraSilva <thanurasilva1989@gmail.com>
2025-02-16 13:44:51 +0530

    Add table for Books and mapping <th> s

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+import { Book } from './components/Book';
 
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Book/>
     </div>
   );
 }
--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,3 +1,37 @@
+import Table from 'react-bootstrap/Table';
 export const Book = ()=>{
-    
+     const tHeadings:string [] = [
+        "BookId",
+        "Title",
+        "Publisher",
+        "ISBN",
+        "Author",
+        "Edition",
+        "Price",
+        "Total Qty",
+        "Avl Aty",
+        "Last Updated Date",
+        "Last Updated Time"
+     ]
+     return(
+         <>
+         <Table striped bordered hover>
+      <thead>
+         <tr>
+            {tHeadings.map((headings)=> (
+                <th>{headings}</th>
+            ))}
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          {/* <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td> */}
+        </tr>
+      </tbody>
+    </Table>
+         </>
+     )
 }
